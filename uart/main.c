223a7ed2006b9c1ec0ec453a1204064b45264d7f
Feng Liu <tycholiu@ijidou.com>
2015-06-28 21:35:27 +0800

    1, [uart]refine code

--- uart/main.c
+++ uart/main.c
@@ -1,31 +1,13 @@
 /**
-  ******************************************************************************
-  * @file    Project/main.c 
-  * @author  MCD Application Team
-  * @version V2.2.0
-  * @date    30-September-2014
-  * @brief   Main program body
-   ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */ 
-
-
+ * STM8 study board
+ * UART3: print debug info
+ *   PD5: TX-----connect to (USB2Serial)write
+ *   PD6: RX-----connect to (USB2Serial)green
+ * UART1: communication
+ *   PA5: TX
+ *   PA4: RX
+ *
+ */
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
 #include "stdio.h"
@@ -33,14 +15,26 @@
 #include "stm8s207R8.h"
 #include "prohead.h"
 
-
 #define UNIQUE_ID_START_ADDR 0x48CD
+
+#define FREQ            2000000
+#define UART1_BAUDRATE   115200
+#define UART3_BAUDRATE   115200
+//used for init UART1 non-lib way
+enum
+{
+		BAUDRATE_HSI1_BRR2 = ((FREQ/UART1_BAUDRATE)/4096)|((FREQ/UART1_BAUDRATE)%16),
+		BAUDRATE_HSI1_BRR1 = (FREQ/UART1_BAUDRATE)/16
+};
 
 u8 i;
 s16 temp;
 u8 t;
 u8 *pUniqueId;
 char buf[128];
+
+void  delayms(u16 Time);
+void  delay(u16 Time);
 
 /* Private defines -----------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
@@ -58,6 +52,44 @@ char getchar(void) {
 }
 */
 
+static void init_clock(void) {
+  CLK_Source_TypeDef clk_source;
+  uint32_t clk_freq;
+
+  delayms(80);
+
+	//default is 2M
+  CLK_HSICmd(ENABLE);
+  //set HSI to 16M
+  //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+  //set HSI to 8M
+  //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV2);
+  //default HSI is 2M ?
+
+  //blow codes failed to enable HSE, but can not delete
+  //CLK_HSECmd(ENABLE);
+  //while((CLK->ECKR & (u8)CLK_FLAG_HSERDY) == RESET);
+
+  clk_source = CLK_GetSYSCLKSource();
+  clk_freq = CLK_GetClockFreq();
+  //set break point here to check the clk_source and clk_freq
+  nop();
+}
+
+void print_clock(void)
+{
+  CLK_Source_TypeDef clk_source;
+  uint32_t clk_freq;
+  clk_source = CLK_GetSYSCLKSource();
+  clk_freq = CLK_GetClockFreq();
+  printf("clk_source = %02X\r\n", clk_source);
+  printf("clk_freq   = %02X\r\n", clk_freq);
+  printf("clk_freq   = %X\r\n", clk_freq);	//clk_freq = 1E, 2000000 = 0x001E8480
+  printf("clk_freq   = %d\r\n", clk_freq);	//clk_freq = 30
+  printf("clk_freq   = %i\r\n", clk_freq);	//clk_freq = 30
+  printf("clk_freq   = %l\r\n", clk_freq);	//clk_freq = ""
+}
+
 char putchar(char c) {
 	UART3_SendData8(c);
 	while(UART3_GetFlagStatus(UART3_FLAG_TXE) == RESET);
@@ -68,39 +100,6 @@ char putchar(char c) {
 	return c;
 }
 
-void Driver_UARTON(void)
-{
-		//UART
-		#define BAUDRATE 115200
-		enum
-		{	
-				BAUDRATE_HSI1_BRR2 = ((2000000/BAUDRATE)/4096)|((2000000/BAUDRATE)%16),
-				BAUDRATE_HSI1_BRR1 = (2000000/BAUDRATE)/16
-		};
-		//¿ªUART1Ê±ÖÓ
-		CLK_PCKENR1 |= SETBIT2;
-		//½ûÖ¹ÊÕ·¢
-		UART1_CR2 &= CLRBIT2;
-		UART1_CR2 &= CLRBIT3;
-		//²¨ÌØÂÊÉè¶¨ Ö÷Ê±ÖÓÎªHSI-16M 
-		UART1_BRR2 = BAUDRATE_HSI1_BRR2; 
-		UART1_BRR1 = BAUDRATE_HSI1_BRR1;
-		//8Î»Êý¾ÝÎ» 1Î»ÆðÊ¼Î» 1Î»Í£Ö¹Î» ÎÞÐ£Ñé
-		UART1_CR1 &= CLRBIT4;
-		UART1_CR3 &= CLRBIT4;
-		UART1_CR3 &= CLRBIT5;
-		UART1_CR1 &= CLRBIT2;
-		//¹Ø±Õ·¢ËÍÖÐ¶Ï
-		UART1_CR2 &= CLRBIT6;
-		//ÔÊÐí½ÓÊÕÖÐ¶Ï
-	  UART1_CR2 |= SETBIT5;
-		//Ê¹ÄÜUART
-		UART1_CR1 &= CLRBIT5;
-		//Æô¶¯ÊÕ·¢
-		UART1_CR2 |= SETBIT2;
-		UART1_CR2 |= SETBIT3;
-}
-
 void Driver_UARTSendByByter(u8 Data)
 {
 		while((UART1_SR&CHSBIT7)==0x00);
@@ -109,10 +108,27 @@ void Driver_UARTSendByByter(u8 Data)
 		printf("send '%c'\n", Data);
 }
 
+/**
+ * used for printf
+ */
+void init_UART3_lib(void) {
+	UART3_Init(
+					UART3_BAUDRATE,
+					UART3_WORDLENGTH_8D,
+					UART3_STOPBITS_1,
+					UART3_PARITY_NO,
+					UART3_MODE_TXRX_ENABLE);
+
+  //UART3_ClearITPendingBit(UART1_IT_RXNE);
+}
+
+/**
+ * Used for communication
+ */
 void init_UART1_lib(void) {
 
 		UART1_Init(
-		115200,
+		UART1_BAUDRATE,
 		UART1_WORDLENGTH_8D,
 		UART1_STOPBITS_1,
 		UART1_PARITY_NO,
@@ -128,31 +144,44 @@ void init_UART1_lib(void) {
 		Driver_UARTSendByByter('A');
 }
 
+/**
+ * Used for communication
+ */
 void init_UART1(void) {
 	_asm("rim");
-	Driver_UARTON();
+
+	//ï¿½ï¿½UART1Ê±ï¿½ï¿½
+	CLK_PCKENR1 |= SETBIT2;
+	//ï¿½ï¿½Ö¹ï¿½Õ·ï¿½
+	UART1_CR2 &= CLRBIT2;
+	UART1_CR2 &= CLRBIT3;
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½è¶¨ ï¿½ï¿½Ê±ï¿½ï¿½ÎªHSI-16M
+	UART1_BRR2 = BAUDRATE_HSI1_BRR2;
+	UART1_BRR1 = BAUDRATE_HSI1_BRR1;
+	//8Î»ï¿½ï¿½ï¿½ï¿½Î» 1Î»ï¿½ï¿½Ê¼Î» 1Î»Í£Ö¹Î» ï¿½ï¿½Ð£ï¿½ï¿½
+	UART1_CR1 &= CLRBIT4;
+	UART1_CR3 &= CLRBIT4;
+	UART1_CR3 &= CLRBIT5;
+	UART1_CR1 &= CLRBIT2;
+	//ï¿½Ø±Õ·ï¿½ï¿½ï¿½ï¿½Ð¶ï¿½
+	UART1_CR2 &= CLRBIT6;
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ð¶ï¿½
+	UART1_CR2 |= SETBIT5;
+	//Ê¹ï¿½ï¿½UART
+	UART1_CR1 &= CLRBIT5;
+	//ï¿½ï¿½ï¿½ï¿½ï¿½Õ·ï¿½
+	UART1_CR2 |= SETBIT2;
+	UART1_CR2 |= SETBIT3;
+
 	Driver_UARTSendByByter('A');
 }
 
 void main(void)
 {
 	//GPIO_Init(GPIOG, GPIO_PIN_ALL, GPIO_MODE_OUT_PP_HIGH_SLOW);
-	
-	//used for printf
-	UART3_Init(
-					115200,
-					UART3_WORDLENGTH_8D,
-					UART3_STOPBITS_1,
-					UART3_PARITY_NO,
-					UART3_MODE_TXRX_ENABLE);
-
-  //UART3_ClearITPendingBit(UART1_IT_RXNE);
-	
-	//used for transfer
-	/*
-
-	*/
-	
+	init_clock();
+	init_UART3_lib();
+	print_clock();
 	/*
 	printf("My id is: \n");
 	pUniqueId = (u8 *)UNIQUE_ID_START_ADDR;
@@ -165,26 +194,32 @@ void main(void)
 	*/
 	
   //init_UART1();
-	
-	//init_UART1_lib();
-
-  Driver_UARTSendByByter('A');
+  //init_UART1_lib();
+  //Driver_UARTSendByByter('A');
 
 	while (1){
-		
 		//Driver_UARTSendByByter('A');
-
-	  //t = getchar();
-		//printf("input is %c\n", t);
-		
-		//gets(buf);
-		//printf("input is %s\n", buf);
-		
-		//gets(buf);
-		//printf("input is %s\n", buf);
+		delay(2000000);	//less than 1s
+		putchar('.');
 	}
 }
 	
+void  delayms(u16 Time)
+{
+  u16 i,j;
+  for(i = 0; i < Time; i++)
+    for(j = 0; j < 340; j++)
+    {
+      nop();
+    }
+}
+
+void delay(u16 Time)
+{
+  u16 i;
+  for(i = 0; i < Time; i++);
+}
+
 #ifdef USE_FULL_ASSERT
 
 /**
